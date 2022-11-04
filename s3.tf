7adc6936cdf28ff174b0eb88b063e75940edc7a8
Dijala Feludu <dijal.feludu@gmail.com>
2022-11-03 19:43:25 -0500

    block public acl

--- s3.tf
+++ s3.tf
@@ -6,7 +6,9 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
+resource "aws_s3_bucket_public_access_block" "access_good_1" {
+  bucket = aws_s3_bucket.bucket_good_1.id
+
+  block_public_acls   = true
+  block_public_policy = true
 }
